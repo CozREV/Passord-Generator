8faf2ec8a1902384a9db3f32448ae7f0a8d7f960
CozREV <kristianafg@gmail.com>
2026-02-06 14:52:13 +0100

    maybe works

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",]
 
-let randomPassword = document.querySelector(".randomPass")
-let password = ""
-let hasPassword = false
+let randomPasswordOne = document.querySelector("#randomPassOne")
+let randomPasswordTwo = document.querySelector("#randomPassTwo")
+password = ""
 
 
-function hasPassword() {
-    if 
-}
 
 
 function generatePassword() {
     for (let i = 0; i < 15; i++) {
         let length = 15
         let randomNumber = characters[Math.floor(characters.length * Math.random())]
-        console.log(randomNumber)
         password += randomNumber
     }
-    randomPassword.textContent = password
+
+    randomPasswordOne.textContent = password
+    password = ""
+
+    for (let i = 0; i < 15; i++) {
+        let length = 15
+        let randomNumber = characters[Math.floor(characters.length * Math.random())]
+        password += randomNumber
+    }
+
+    randomPasswordTwo.textContent = password
+    password = ""
 }
